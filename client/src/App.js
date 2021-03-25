71ab91f76455ce00f7e1b3fb3e07f22bd9ef99bc
Josh Uvi <joshuauvbiekpahor@gmail.com>
2021-03-25 12:45:52 +0000

    change some things

--- client/src/App.js
+++ client/src/App.js
@@ -9,24 +9,20 @@ import './App.css';
 
 import { Provider } from './context';
 
-class App extends Component {
-  render() {
-    return (
-      <Provider>
-        <Router>
-          <React.Fragment>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Index} />
-                <Route exact path='/lyrics/track/:id' component={Lyrics} />
-              </Switch>
-            </div>
-          </React.Fragment>
-        </Router>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Index} />
+            <Route exact path='/lyrics/track/:id' component={Lyrics} />
+          </Switch>
+        </div>
+      </React.Fragment>
+    </Router>
+  </Provider>
+);
 
 export default App;
